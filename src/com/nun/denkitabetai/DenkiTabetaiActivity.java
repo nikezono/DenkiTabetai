b6744067029a486d4880abee3e1005d748467380
Sho Nakazono <nikezono@gmail.com>
2012-10-29 22:29:30 +0900

    first commit

---
+++ src/com/nun/denkitabetai/DenkiTabetaiActivity.java
@@ -0,0 +1,22 @@
+package com.nun.denkitabetai;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import org.apache.cordova.DroidGap;
+
+public class DenkiTabetaiActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    //@Override
+    //public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.activity_denki_tabetai, menu);
+      //  return true;
+    //}
+}
