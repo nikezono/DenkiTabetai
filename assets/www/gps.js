28f594cd18bc0bc1f3f67fc292b42dc672e1efa6
Sho Nakazono <nikezono@gmail.com>
2012-10-30 00:16:55 +0900

    gps

--- assets/www/gps.js
+++ assets/www/gps.js
@@ -3,23 +3,32 @@
  * DenkiTabetai.apk
  * com.nun.DenkiTabetai
  * GPSの値を取得するjavascript
- * getGps()でGPSの値を取得してアクセサメソッドで値をとってくる
+ * GPS gps = new GPS;
+ * gps.getGps()でGPSの値を取得する
+ * gps.latとかで緯度経度を取り出す
  */
 
+class GPS(){
 
-function onSuccess(position) {
-    var lat = position.coords.latitude;
-    var lng = position.coords.longitude;
-    var alt = position.coords.altitude;
-    var hdng = position.coords.heading;
-    //var t = new Date(position.timestamp);現在時刻
-};
+	var lat,lng,alt,hdng;
+	
+	function onSuccess(position) {
+		lat = position.coords.latitude;
+    	lng = position.coords.longitude;
+    	alt = position.coords.altitude;
+    	hdng = position.coords.heading;
+    	//var t = new Date(position.timestamp);現在時刻
+    	console.log(lat);
+    	alert(lat);
+    	
+	};
  
-function onError(error) {
-    alert('code: ' + error.code + ':' + error.message);
+	function onError(error) {
+    	alert('code: ' + error.code + ':' + error.message);
+	}
+ 
+	function getGPS(){
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+	}
+	
 }
- 
-function getGps(){
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-}
- 
