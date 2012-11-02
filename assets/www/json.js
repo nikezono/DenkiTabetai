1892b7b1f313f691a1c8a6a6f5db3a56d5e569ee
Sho Nakazono <nikezono@gmail.com>
2012-11-02 13:35:58 +0900

    nouki-okure

---
+++ assets/www/json.js
@@ -0,0 +1,17 @@
+var res;
+var latitude, longitude;
+  $(function() {
+    
+    latitude = "35.681382";
+    longitude = "139.766084";
+    navigator.geolocation.getCurrentPosition(function() {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longtitude;
+    },function(){
+    	//alert("GPS error");
+    });
+    $.getJSON("http://ngkr.net:3000/?lat=" + latitude + "&lng=" + longitude, function(json) {
+    	res = json;
+    }).success(setList);
+    
+  });
--- assets/www/list.js
+++ assets/www/list.js
@@ -1,16 +1,67 @@
-(function() {
-  $(function() {
-    var latitude, longitude;
-    latitude = "35.396221";
-    longitude = "139.466404";
-    navigator.geolocation.getCurrentPosition(function() {
-      latitude = position.coords.latitude;
-      return longitude = position.coords.longtitude;
-    });
-    return $.getJSON("http://ngkr.net:3000/?lat=" + latitude + "&lng=" + longitude, function(json) {
-      alert(json);
-      alert(json.results);
-      return alert(json.results[1].title);
-    });
-  });
-}).call(this);
+/* 
+*  list.js
+*  com.nun.denkitabetai
+*  index.html#listにリストを追加していく
+*/
+
+function setList(){
+	/*
+	 * もし、随時通信を行うなら以下の書き方が正しい
+	 * $(a#list).click(function(){})
+	 */
+	
+	//とりあえず、ライブラリのロード時に自動でlist.htmlにappendしていくようにする
+ // $("a#btnList").click(function(){
+	  
+
+		//もらったjsonファイルを距離順に並べ替える
+		for(var num in res.results){
+			//console.log(res.results[num].latitude,longitude);
+			var dis = getLength(res.results[num].latitude,res.results[num].longitude,latitude,longitude);
+			res.results[num].distance = dis;
+		}
+		res.results.sort(sort_by('distance', false, parseInt));
+		//console.log(res.results);
+	  
+	  
+	  if(res.results.length == 0){
+		  $("#contentLi").text();
+		  alert("現在位置周辺に利用可能な電源カフェはありません");
+	  }else{
+	  for(var num in res.results){
+    	
+	  	$("#li").append(
+  			"<li><a href='" + res.results[num].url_pc + "'>"
+	  			+ res.results[num].title + "</a>" +
+  			"<span class='ui-li-aside' style=' text-align:left;font-weight:lighter;font-style:italic;'>" + 
+  			res.results[num].category[0] +  "," + res.results[num].category[1] +"</span>" +
+  			"<span class='ui-li-count'>"+res.results[num].powersupply +"</span>" +
+  			"</li>"
+     	);
+	  	}
+	  }
+  //});
+}
+
+var sort_by = function(field, reverse, primer){
+	   reverse = (reverse) ? -1 : 1;
+	   return function(a,b){
+	       a = a[field];
+	       b = b[field];
+	       if (typeof(primer) != 'undefined'){
+	           a = primer(a);
+	           b = primer(b);
+	       }
+	       if (a<b) return reverse * -1;
+	       if (a>b) return reverse * 1;
+	       return 0;
+	   }
+	}
+
+function getLength(x1 , y1 , x2 , y2 ) {
+	　　var pointLength;
+	　　pointLength = Math.sqrt(Math.pow(x2 - x1 , 2 ) + 
+	　　　　　　　　　　　　　　　　　　　　Math.pow(y2 - y1 , 2 )); 
+	　　return pointLength;
+	}
+
