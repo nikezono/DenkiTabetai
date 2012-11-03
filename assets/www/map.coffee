34edae4192cf3da0e8c3e1e67c1621d03b478791
masaya <t09090mu@sfc.keio.ac.jp>
2012-11-03 15:46:54 +0900

    fix

--- assets/www/map.coffee
+++ assets/www/map.coffee
@@ -1,9 +1,16 @@
 $ ->
 
-  do ->
-    myLating = new google.maps.LatLng(-39.397);
+  mapCanvas = ->
+    myLating = new google.maps.LatLng(36.230672,137.963916)
     myOptions =
-      zoom: 8
+      zoom: 15
       center: myLating
+      mapTypeControl: false
+      navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL}
       mapTypeId: google.maps.MapTypeId.ROADMAP
+    map = new google.maps.Map(document.getElementById("map"),myOptions)
 
+    pageHeight = $(document).height()
+    $("#map").css "height", pageHeight
+
+  $("#mapLoad").bind "pageshow",mapCanvas
--- assets/www/map.js
+++ assets/www/map.js
@@ -1,13 +1,22 @@
 (function() {
   $(function() {
-    return (function() {
-      var myLating, myOptions;
-      myLating = new google.maps.LatLng(-39.397);
-      return myOptions = {
-        zoom: 8,
+    var mapCanvas;
+    mapCanvas = function() {
+      var map, myLating, myOptions, pageHeight;
+      myLating = new google.maps.LatLng(36.230672, 137.963916);
+      myOptions = {
+        zoom: 15,
         center: myLating,
+        mapTypeControl: false,
+        navigationControlOptions: {
+          style: google.maps.NavigationControlStyle.SMALL
+        },
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
-    })();
+      map = new google.maps.Map(document.getElementById("map"), myOptions);
+      pageHeight = $(document).height();
+      return $("#map").css("height", pageHeight);
+    };
+    return $("#mapLoad").bind("pageshow", mapCanvas);
   });
 }).call(this);
