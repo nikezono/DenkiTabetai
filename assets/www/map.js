e5b3683a3959e1a12760953cd02f6dac9d9fe978
masaya <t09090mu@sfc.keio.ac.jp>
2012-11-03 14:21:04 +0900

    fix

--- assets/www/map.js
+++ assets/www/map.js
@@ -1,48 +1,13 @@
-// -*- coding: utf-8 -*-
-
-(function($) {
-    var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-    var map;
-    var marker;
-    var currentLocation;
-
-    $(function() {
-    	$(document).bind("deviceready", initializeMap);
-    	$("#reflesh").click(function() { 
-    		setCurrentLocation();
-    	});
-    });
-
-    function onError(e) {
-    	alert('code: '    + e.code    + '\n' +
-    			'message: ' + e.message + '\n');
-    }
-
-    function initializeMap() {
-    	navigator.geolocation.getCurrentPosition(function(position) {
-	    currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	    var options = {
-		zoom: 10, center: currentLocation,
-		disableDefaultUI: true, mapTypeId: google.maps.MapTypeId.ROADMAP
-	    };
-	    map = new google.maps.Map($("#map_canvas").get(0), options);
-	    map.setCenter(currentLocation);
-	    directionsDisplay.setMap(map);
-	    marker = new google.maps.Marker({
-		position: currentLocation,
-		map: map, title: "Current Location"
-	    });
-	}, onError);
-    }
-
-    function setCurrentLocation() {
-	navigator.geolocation.getCurrentPosition(function(position) {
-	    currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	    map.setCenter(currentLocation);
-	    marker.setPosition(currentLocation);
-	}, onError);
-    }
-
-
-})(jQuery)
+(function() {
+  $(function() {
+    return (function() {
+      var myLating, myOptions;
+      myLating = new google.maps.LatLng(-39.397);
+      return myOptions = {
+        zoom: 8,
+        center: myLating,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+    })();
+  });
+}).call(this);
